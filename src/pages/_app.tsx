d84f22365c247697ad011f17173cdd856f04318e
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-09-13 18:17:20 +0800

    Add favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title> We are the Champions </title>
+        <link rel="icon" href="https://fav.farm/⚽" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
